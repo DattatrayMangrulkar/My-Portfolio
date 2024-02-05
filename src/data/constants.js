c66c4a2e2b4c34a862ebec93a5a5307074a4f31e
DattatrayMangrulkar <dattatray.m.2496@gmail.com>
2024-02-06 01:08:20 +0530

    commiting source code of portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,344 @@
+
+export const Bio = {
+  name: "Dattatray Mangrulkar",
+  roles: [
+    "Full Stack Developer  < / > ",
+    "Software Developer ",
+    "Backend Developer  ^_^ ",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/DattatrayMangrulkar",
+  resume:
+    "https://drive.google.com/file/d/1YAenY1-_pGmF7CFx1rrgJvU6V4zOpgJf/view?usp=drive_link",
+  linkedin: "www.linkedin.com/in/dattatray-mangrulkar-944b5127b",
+};
+
+export const skills = [
+  // FRONTEND SKILLS..
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image: "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      }
+    ],
+  },
+    // BACKEND SKILLS..
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/300px-Java_programming_language_logo.svg.png"
+          
+      },
+      {
+        name: "SpringFramework",
+        image: "https://www.bridgetech.co.id/storage/2021/02/spring-logo.png",
+          
+      },
+      {
+        name: "SpringBoot",
+        image: "https://pbs.twimg.com/profile_images/1235868806079057921/fTL08u_H_400x400.png"
+          
+      },
+      {
+        name: "Microservices",
+        image: "https://miro.medium.com/v2/resize:fit:693/0*5t8MYXpyEXclubeT.jpg"
+          
+      },
+      {
+        name: "RESTAPI",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcWnOIf2ZGZb-lN1B88btRmX60CrMUWvxGgg&usqp=CAU"
+          
+      },
+      {
+        name: "Hibernate",
+        image: "https://lh3.googleusercontent.com/proxy/KVji5LqHyOY8Hc8EJgNdrDF1OJkkQjr27vRzKwRscIcCR6HWEH-MIFz8D_n6THazOOHAbnkn4YDuXdEsPVfN7wyHKlm6a7Cus4bddtW_P1IPSAJ7Z7q5ODMIOkQ"
+          
+      },
+      {
+        name: "JDBC",
+        image: "https://v32.openhab.org/logos/jdbc.png"
+          
+      },
+      {
+        name: "Servlet",
+        image: "https://www.sitesbay.com/servlet/files/sublogo.png"
+          
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Agile Methodology",
+        image: "https://w7.pngwing.com/pngs/146/747/png-transparent-agile-project-management-agile-software-development-project-management-text-logo-business.png"
+          
+      },
+      {
+        name: "SDLC",
+        image: "https://miro.medium.com/v2/resize:fit:1400/1*i3SdOcCP-ct5U7Qo_vBm_w.png"
+          
+      },
+    ],
+  },
+ // OTHERS SKILLS..
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Eclipse",
+        image: "https://user-images.githubusercontent.com/11943860/46922575-7017cf80-cfe1-11e8-845a-0cd198fb546c.png",
+
+      },
+      {
+        name: "STS",
+        image: "https://www.bridgetech.co.id/storage/2021/02/spring-logo.png",
+
+      },
+      {
+        name: "MYSQL Workbench",
+        image: "https://img.utdstc.com/icon/f6f/11c/f6f11c75fda63dd454fa5db9610a77cfd6752be4db11010f2e4252551a4abccd:200",
+
+      },
+      {
+        name: "MongoDB Compass",
+        image: "https://user-images.githubusercontent.com/12401985/69677784-80bec400-1082-11ea-89b2-b2120eb84676.png",
+
+      },
+      {
+        name: "VS Code",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image: "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Netlify",
+        image: "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.perditasoft.com/img/facts-img.png",
+    role: "Junior Software Developer",
+    company: " Perdita Software technologies Pvt. Ltd.",
+    date: "April 2023 - Present",
+    desc: "Merchant Dashboard is billdesk application for the merchant onboarding process.  It helps the admin to onboard the customer on billdesk online payment which enables banking and merchant website transactions.This application also aids the admin can able to manage the users and customers.",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "Hibernate",
+      "Spring Security",
+      "Payment Gateway",
+      "REST API",
+      "MySQL",
+      "ReactJS",
+      "JavaScript",
+      "Agile Methologies",
+      
+    ],
+    doc: "",
+    // doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/d/db/C-DAC_LogoTransp.png",
+    school: "C-DAC Thiruvananthapuram",
+    date: "September 2022 - March 2023",
+    grade: "77.00%",
+    desc: "I have recently completed my PG Diploma In Advanced Computing from Centre for Development of Advanced Computing (C-DAC). I have taken courses in Data Structures Algorithms, Object-Oriented Programming, Database Management Systems, web based java programming, web programming technologies and Concept of programming & Operating Systems, among others.",
+    degree: "PG Diploma In Advanced Computing (PG-DAC)",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5fwtBZzXDqt-JhE1PgD01fYPev_24wwOMqnx4kyTgj4scG0cZfeHJgfgEOqI8wDQmOdM&usqp=CAU",
+    school: "Dr.D.Y.Patil Institute of Engineering & Technology, Pune",
+    date: "July 2016 - July 2019",
+    grade: "8.32 CGPA",
+    desc: "I have completed my Bachelor of Engineering at Dr.D.Y.Patil Institute of Engineering & Technology, Pune,  where I studied the foundation and practical oriented knowledge about civil engineering and technology. also i have completed some softwares related to civil works like AutoCAD, StaadPro, ETabs and Revit Architecture.",
+    degree: "Bachelor of Engineering - BE, Civil Engineering",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm4Au8BDz7WhAp0xk2iysWDBfNtzgZt966kQ&usqp=CAU",
+    school: "A. G. Patil Polytechnic Institute, Solapur",
+    date: "July 2012 - July 2015",
+    grade: "81.21%",
+    desc: "I have completed my Diploma education at A. G. Patil Polytechnic Institute, Solapur,  where I studied the foundation of civil engineering",
+    degree: "Diploma In Civil Engineering",
+  },
+  {
+    id: 3,
+    img: "https://askusedu.com/wp-content/uploads/2023/03/logo-39-300x300.jpg",
+    school: "Bharati Vidyapeeth Madhyamik Vidhyalay School, Solapur ",
+    date: "March 2003 - March 2012",
+    grade: "75.09%",
+    desc: "I have completed my class School education at Bharati Vidyapeeth Madhyamik Vidhyalay School, Solapur",
+    degree: "Secondary School Certificate (SSC)", 
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Hospital Management System",
+    date: " February 2023 – March 2023",
+    description:
+      "Hospital Management System is a robust and comprehensive application designed to streamline and optimize the administrative and operational aspects of healthcare facilities. This advanced system integrates cutting edge technologies to provide a seamless experience for both healthcare professionals and administratives taff.",
+    image: "https://media.istockphoto.com/id/1440001176/photo/administrator-with-medical-team.jpg?s=1024x1024&w=is&k=20&c=dtkEn1KqQHTJgx4LxCCoHWPYR3M4AImd0nw7Rq31H6E=",
+    tags: ["Java, Spring boot, Spring Security, Hibernate, MySQL, ReactJS, JavaScript, HTML, CSS"],
+    category: "Web Application",
+    github: "https://github.com/DattatrayMangrulkar/Hospital-Management-System",
+    webapp: "https://dattatray-hms.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "EDOC - Project",
+    date: "August 2023 – September 2023",
+    description:
+      "The system provides a centralized platform for students and schools for documentation and events promotion. The lethargic workload of submission of documents at the time of admission is reduced. Students will be aware of organized events in different school so that they can participate and get certificates for it.",
+    image: " https://www.shutterstock.com/image-photo/paperless-workplace-idea-esigning-electronic-600nw-2064550976.jpg",
+    tags: ["Java, Spring boot, Spring Security, Hibernate, MySQL, ReactJS, JavaScript, HTML, CSS"],
+    category: "Web Application",
+    github: "https://github.com/DattatrayMangrulkar/EDOC-Project",
+    webapp: "https://dattatray-edoc.netlify.app",
+  },
+  {
+    id: 2,
+    title: "Online Restaurant Management System",
+    date: "November 2023 – December 2023",
+    description:
+      "An Online Restaurant Management System aims to enhance the efficiency of restaurant operations, improve customer experience, and provide valuable insights to restaurant owners for strategic decision-making. This comprehensive system contributes to the overall digital transformation of the restaurant industry, offering a seamless and modern dining experience.",
+    image: " https://media.istockphoto.com/id/1094919124/photo/male-manager-and-female-chef-using-digital-tablet-in-kitchen.jpg?s=612x612&w=0&k=20&c=HN8Z4B6i1l8lSY__6qaVZQMk88yO0TEWvYERpCtW3P0= ",
+    tags: ["Java, Spring boot, Spring Security, Hibernate, MySQL, ReactJS, JavaScript, HTML, CSS"],
+    category: "Web Application",
+    github: "",
+    webapp: "",
+  },
+
+  {
+    id: 3,
+    title: "Merchant Dashboard is billdesk application",
+    date: "july 2023 – Present",
+    description:
+      " Merchant Dashboard is billdesk application for the merchant onboarding process. It helps the admin to onboard the customer on billdesk online payment which enables banking and merchant website transactions. This application also aids the admin can able to manage the users and customers. ",
+    image: "https://www.arnavsameer.com/wp-content/uploads/2021/05/BillDesk-OneView-Dashboard.png ",
+    tags: ["Java, Spring boot, Spring Security, Payment Gateways, Hibernate, MySQL, ReactJS, JavaScript, HTML, CSS"],
+    category: "Web Application",
+    github: "",
+    webapp: "",
+  },
+];
+
+
+export const certificate = [
+  {
+    id: 0,
+    title: "Learn Jaava Programming Beginner To Master",
+    date: " December 2022",
+    description: "The course consists of JAVA features, Java SE Concept of programming are made simple and easy. By the end of the course you will understand Java extremely well and will be able to build your own Java applications. After completion of the course, you will be as productive as a software developer. ",
+    github: "https://github.com/DattatrayMangrulkar/Certifications/tree/certifications",
+  },
+
+  {
+    id: 1,
+    title: "Git & GitHub The Practical Guide",
+    date: " December 2023",
+    description: "Learn all you need to work with Git, including all about repositories, commits, branches & more. Dive into advanced Git features like merging, rebasing, the stash and learn how to manage merge conflicts. Git (a version control system) and Github (a cloud provider for Git managed projects) form an outstanding combination to provide the best possible experience to create and maintain a clearly structured project history. This course guides everyone (no prior knowledge is required) through the core steps to use these tools in your daily projects with ease. ",
+    github: "https://github.com/DattatrayMangrulkar/Certifications/tree/certifications",
+  },
+
+  {
+    id: 2,
+    title: "The Ultimate MySQL Bootcamp:",
+    date: " November 2023",
+    description: " This course was just completely redone and rebuilt from the ground up, with over 325 brand new videos recorded. The course now uses MySQL 8.x and covers new topics including: Window Functions, Views, and SQL modes. ",
+    github: "https://github.com/DattatrayMangrulkar/Certifications/tree/certifications",
+  },
+
+  {
+    id: 3,
+    title: "MongoDB The Complete Developers Guide",
+    date: " November 2023",
+    description: " Learn what document databases are and how data is organized with MongoDB Learn how to perform CRUD operations with MongoDB Don't stop at the basics - learn all about writing complex MongoDB queries, in-depth and with practical examples. Write efficient and well-performing queries to fetch data in the format you need it ",
+    github: "https://github.com/DattatrayMangrulkar/Certifications/tree/certifications",
+  },
+
+  {
+    id: 4,
+    title: "Spring Security Zero To Master along with JWT",
+    date: " January 2024",
+    description: " Spring Security framework details and it features. How to adapt security for a Java web application using Spring Security What is CSRF, CORS, JWT Applying authorization rules using roles, authorities inside a web application using Spring Security Method level security in web/non-web applications ",
+    github: "https://github.com/DattatrayMangrulkar/Certifications/tree/certifications",
+  },
+
+  {
+    id: 5,
+    title: "Spring Boot 3, Spring 6 & Hibernate For Beginners",
+    date: " December 2023",
+    description: " LEARN key Spring Boot 3 features: Core, Annotations, Java Config, Spring REST, Spring MVC, AOP, Hibernate/JPA and Maven. Spring Boot 3 REST API (with full database CRUD real-time project).Spring Boot 3 REST API Security (with password encryption in the database). Spring Boot 3 with JPA and Spring Data JPA (with full database CRUD real-time project). Spring Boot 3 with Spring Data REST (with full database CRUD real-time project). Spring Boot 3 with Spring MVC, Thymeleaf (with full database CRUD real-time project). ",
+    github: "https://github.com/DattatrayMangrulkar/Certifications/tree/certifications",
+  },
+
+  {
+    id: 6,
+    title: "Centre for Development of Advanced Computing (C-DAC)",
+    date: " March 2023",
+    description: " Centre for Development of Advanced Computing (C-DAC). This Diploma in Advanced Computing (PG-DAC) is a post graduate program and is the flagship programme of ACTS. The course is targeted towards engineers and IT professionals who wish to venture into the domain of advanced computing. The course aims to groom the students to enable them to work on current technology scenarios as well as prepare them to keep pace with the changing face of technology and the requirements of the growing IT industry. The course curriculum has been designed keeping in view the emerging trends in advanced computing as well as contemporary and futuristic human resource requirements of the ICT industry. ",
+    github: "https://github.com/DattatrayMangrulkar/Certifications/tree/certifications",
+  },
+
+];
+
+
+export const TimeLineData = [
+  { year: 2023, text: "Started my journey" },
+  { year: 2024, text: "Worked as a software developer" },
+
+];
